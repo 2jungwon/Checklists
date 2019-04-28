35435c526253cc2e5eaecfed5bf88fe9394e5b2a
2jungwon <james66won@gmail.com>
2019-04-28 20:51:32 +0900

    tableView1 completed

---
+++ Checklists/ChecklistViewControlloer.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by 이정원 on 28/04/2019.
+//  Copyright © 2019 Jungwon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+}
+
